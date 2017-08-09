d921a5bbc9afad54e73d01ca9563d4e37f40f219
wyj <ismrwang@gmail.com>
2017-08-09 16:04:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" @click="showToast">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Toast from './components/toast';
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  methods: {
+    showToast () {
+      console.log(Toast)
+      Toast('Upload Completed');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
